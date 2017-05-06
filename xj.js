bd90ba2bd802ce7ab67743295b0cb625fd2b7221
xujian <x8730490@163.com>
2017-05-06 18:04:29 +0800

    express demo

--- xj.js
+++ xj.js
@@ -1,1 +1,10 @@
-123
+var express = require("express");
+var app = express();
+app.get("", (req, res) => {
+    res.send("欢迎来到首页");
+});
+var server = app.listen(3000, () => {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
